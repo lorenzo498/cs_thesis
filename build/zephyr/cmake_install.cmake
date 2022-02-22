2e03dd3c6b8b8457152652fce6cfe5bbb1d6711b
lorenzo498 <lorenzo.brancaleoni@studenti.unicam.it>
2022-02-22 15:09:59 +0100

    lettura del qr code riuscita

---
+++ build/zephyr/cmake_install.cmake
@@ -0,0 +1,85 @@
+# Install script for directory: /opt/nordic/ncs/v1.7.1/zephyr
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/opt/nordic/ncs/v1.7.1/toolchain")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "TRUE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/opt/nordic/ncs/v1.7.1/toolchain/bin/arm-none-eabi-objdump")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/zephyr/arch/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/zephyr/lib/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/zephyr/soc/arm/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/zephyr/boards/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/zephyr/subsys/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/zephyr/drivers/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/nrf/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/mcuboot/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/trusted-firmware-m/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/cjson/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/openthread/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/pelion-dm/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/cddl-gen/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/memfault-firmware-sdk/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/CANopenNode/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/civetweb/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/cmsis/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/fatfs/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/hal_nordic/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/st/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/libmetal/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/littlefs/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/loramac-node/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/lvgl/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/lz4/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/mbedtls/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/mcumgr/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/mipi-sys-t/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/nanopb/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/nrf_hw_models/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/open-amp/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/segger/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/tinycbor/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/tinycrypt/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/TraceRecorder/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/nrfxlib/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/modules/connectedhomeip/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/zephyr/kernel/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/zephyr/cmake/flash/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/zephyr/cmake/usage/cmake_install.cmake")
+  include("/Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/build/zephyr/cmake/reports/cmake_install.cmake")
+
+endif()
+
---
+++ build/zephyr/nrf9160dk_nrf9160.dts.pre.d
@@ -0,0 +1,15 @@
+empty_file.o: /opt/nordic/ncs/v1.7.1/zephyr/misc/empty_file.c \
+ /opt/nordic/ncs/v1.7.1/zephyr/boards/arm/nrf9160dk_nrf9160/nrf9160dk_nrf9160.dts \
+ /opt/nordic/ncs/v1.7.1/zephyr/dts/arm/nordic/nrf9160_sica.dtsi \
+ /opt/nordic/ncs/v1.7.1/zephyr/dts/common/mem.h \
+ /opt/nordic/ncs/v1.7.1/zephyr/dts/arm/nordic/nrf9160.dtsi \
+ /opt/nordic/ncs/v1.7.1/zephyr/dts/arm/armv8-m.dtsi \
+ /opt/nordic/ncs/v1.7.1/zephyr/dts/common/skeleton.dtsi \
+ /opt/nordic/ncs/v1.7.1/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ /opt/nordic/ncs/v1.7.1/zephyr/include/dt-bindings/i2c/i2c.h \
+ /opt/nordic/ncs/v1.7.1/zephyr/include/dt-bindings/gpio/gpio.h \
+ /opt/nordic/ncs/v1.7.1/zephyr/dts/arm/nordic/override.dtsi \
+ /opt/nordic/ncs/v1.7.1/zephyr/dts/arm/nordic/nrf9160_common.dtsi \
+ /opt/nordic/ncs/v1.7.1/zephyr/boards/arm/nrf9160dk_nrf9160/nrf9160dk_nrf9160_common.dts \
+ /opt/nordic/ncs/v1.7.1/zephyr/boards/arm/nrf9160dk_nrf9160/nrf9160dk_nrf9160_partition_conf.dts \
+ /Users/lorenzobrancaleoni/Downloads/sdk-nrf-lpuart_using_normal_uart/samples/peripheral/lpuart/boards/nrf9160dk_nrf9160.overlay
